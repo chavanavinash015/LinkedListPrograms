a84b06a1ca245c4b28001303f92fb6d36ddc6259
arc-015 <chavanavinash015@gmail.com>
2022-11-03 16:31:45 +0530

    modify: added create Linkedlist program

--- src/com/bridgelabz/Linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/Linkedlist/LinkedListMain.java
@@ -4,6 +4,7 @@ public class LinkedListMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the linkedList programs");
+        System.out.println("Create Linked List of 56, 30, 70");
 
     }
 }
