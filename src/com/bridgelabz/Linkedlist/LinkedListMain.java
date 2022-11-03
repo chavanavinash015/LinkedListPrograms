9ba858d53b7a01e21ad7c74853a04e1b018812ac
arc-015 <chavanavinash015@gmail.com>
2022-11-03 16:23:44 +0530

    added welcome to Linkedlist program

---
+++ src/com/bridgelabz/Linkedlist/LinkedListMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.Linkedlist;
+
+public class LinkedListMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the linkedList programs");
+
+    }
+}
